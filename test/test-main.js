d0a2b0294bf0247183a50a1f528b02e6fd0e3e01
Dan <danwellman@hotmail.com>
2015-05-31 17:42:26 +0100

    Fix Karma 'no timestamp for...' errors

--- test/test-main.js
+++ test/test-main.js
@@ -11,7 +11,7 @@ for (var file in window.__karma__.files) {
 
 requirejs.config({
   // Karma serves files from '/base'
-  baseUrl: 'base/app',
+  baseUrl: '/base/app',
 
   // ask Require.js to load these files (all our tests)
   deps: tests,
